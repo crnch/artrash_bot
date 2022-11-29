19b903a140b4c86ecf44933d6ac96435d12dfd00
crnch <crnch@proton.me>
2022-11-29 16:22:25 -0500

    Petrus our saviour

--- db.ts
+++ db.ts
@@ -1,10 +1,11 @@
 import { SupabaseClient } from 'https://deno.land/x/supabase@1.3.1/mod.ts'
+import { getEnvVariable} from "./helpers.ts"
+
 
 // Create a single supabase client for interacting with your database
-const url = 'https://vceryglsnfwfhzkccxly.supabase.co/'
 const supabase = new SupabaseClient(
-  url,
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjZXJ5Z2xzbmZ3Zmh6a2NjeGx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc5Mzk5NTUsImV4cCI6MTk4MzUxNTk1NX0.OS7u62T0BmWToti9kSQFillWwMyVM0K82IqxkXZ-K7o',
+  getEnvVariable("SUPABASE_URL")!,
+  getEnvVariable("SUPABASE_SECRET")!,
   { detectSessionInUrl: false },
 )
 
